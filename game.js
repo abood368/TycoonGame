0c95ce21b90a3c501ece2c5255f82448da30f2a5
abood368 <abdulrahmansardani42@gmail.com>
2023-11-11 14:56:38 -0500

    Changed the entire code so that there is no longer a grid and the game works without a grid object, the squares spawn at random locations on the grid, this helps a lot with shortening the code and making it easier to read.

--- game.js
+++ game.js
@@ -3,6 +3,40 @@ let money = 0;
 
 // make the user have money instead just so it is more organized.
 
+const shapes = {
+  list: [],
+  isClicked: function () {
+    for (let shape of this.list) {
+      if (
+        mouse.clickedX > shape.x &&
+        mouse.clickedX < shape.x + shape.size &&
+        mouse.clickedY > shape.y &&
+        mouse.clickedY < shape.y + shape.size
+      ) {
+        if (shape.color === 'rgba(255,255,255,1)' || shape.color === 'white') {
+          if (!shape.clicked) {
+            user.scored();
+            shape.color = 'rgba(0,0,0,1)';
+            shape.draw();
+            mouse.reset();
+          }
+        }
+      }
+    }
+  },
+};
+
+canvas.addEventListener('mousemove', (event) => {
+  mouse.x = event.layerX;
+  mouse.y = event.layerY;
+});
+
+canvas.addEventListener('click', (e) => {
+  mouse.clickedX = e.layerX;
+  mouse.clickedY = e.layerY;
+  shapes.isClicked();
+});
+
 const user = {
   money: 0,
   multiplier: 1,
@@ -25,34 +59,28 @@ const user = {
   },
 };
 
-// function randomize() {
-//   let randomIndex = Math.floor(Math.random() * grid.squares.length);
-//   let randomSquare = grid.squares[randomIndex];
-//   randomSquare.color = 'rgba(255,255,255,1)';
-//   grid.render();
-// }
-
-// let intervalId = setInterval(() => {
-//   grid.clear();
-//   randomize();
-// }, 1000);
-
-// let randomSquare;
-
 function appear() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       clearCanvas();
-      let randomIndex = Math.floor(Math.random() * grid.squares.length);
-      randomSquare = grid.squares[randomIndex];
-      randomSquare.color = 'rgba(255,255,255,1)';
-      grid.render();
-      if (randomSquare.color === 'rgba(255,255,255,1)') {
-        resolve(randomSquare); // return the square to use it in the promise chain.
+      let squareSize = 50;
+      let randomX = Math.random() * (setUpCanvas() - squareSize);
+      let randomY = Math.random() * (setUpCanvas() - squareSize);
+      let square = new Square(
+        randomX,
+        randomY,
+        squareSize,
+        'rgba(255,255,255,1)'
+      );
+      shapes.list.push(square);
+      square.draw();
+      console.log(square.color === 'rgba(255,255,255,1)');
+      if (square.color === 'rgba(255,255,255,1)') {
+        resolve(square); // return the square to use it in the promise chain.
       } else {
         reject('There was an error.');
       }
-    }, 1000);
+    }, 1000); // can replace with a speed variable
   });
 }
 
@@ -60,14 +88,15 @@ function disappear(square) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       clearCanvas();
+      shapes.list.pop();
       square.color = 'rgba(0,0,0,1)';
-      grid.render();
+      square.draw();
       if (square.color === 'rgba(0,0,0,1)') {
-        resolve('it turned black');
+        resolve('It turned black');
       } else {
         reject('error');
       }
-    }, 1000);
+    }, 1000); // can replace with a speed variable
   });
 }
 
@@ -80,12 +109,11 @@ async function playGame() {
         .then(disappear) // the square gets passed in as a parameter for the disappear function.
         .catch((err) => console.log(err));
     }
-    // for (let i = 0; i < 1000; i++) {
-    //   await appear()
-    //   .then(disappear) // the square gets passed in as a parameter for the disappear function.
-    //   .catch((err) => console.log(err));
-    // }
   }
+}
+
+function endGame() {
+  gameStart = false;
 }
 
 // playGame();
--- main.js
+++ main.js
@@ -17,7 +17,7 @@ setUpCanvas(); // render the canvas for the first time
 // All the Event Listeners
 
 window.addEventListener('resize', () => {
-  grid.resize();
+  // grid.resize();
 });
 
 const mouse = {
@@ -33,116 +33,15 @@ const mouse = {
     this.clickedY = undefined;
   },
 };
-canvas.addEventListener('mousemove', (event) => {
-  mouse.x = event.layerX;
-  mouse.y = event.layerY;
-  c.clearRect(0, 0, setUpCanvas(), setUpCanvas());
-  grid.highlight();
-});
-
-canvas.addEventListener('click', (e) => {
-  mouse.clickedX = e.layerX;
-  mouse.clickedY = e.layerY;
-  c.clearRect(0, 0, setUpCanvas(), setUpCanvas());
-  grid.render();
-}); // can we add this to each individual square? (no I tried lol)
-
-const grid = {
-  size: 7,
-  squares: [],
-  squareSize: () => {
-    setUpCanvas(); // This ensures that the square size is relative to the current canvas size.
-    let ss = canvas.width / grid.size;
-    return ss;
-  },
-
-  // Creates the layout of the board and stores it in the squares array.
-
-  create: function () {
-    for (let i = 0; i < this.size; i++) {
-      let row = [];
-      for (let j = 0; j < this.size; j++) {
-        let s = new Square(
-          j * this.squareSize(), // x
-          i * this.squareSize(), // y
-          this.squareSize(), //  size
-          'rgba(0,0,0,1)' // color
-        );
-        this.squares.push(s);
-      }
-    }
-    this.render();
-  },
-
-  // Renders the squares created inside the squares array
-  // always call this function below when any changes are made to a square.
-
-  render: function () {
-    for (let s of this.squares) {
-      s.update();
-    }
-  },
-
-  highlight: function () {
-    for (let s of this.squares) {
-      s.highlight();
-    }
-  },
-
-  // resizes the squares and positions them accordingly.
-
-  resize: function () {
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        const index = i * this.size + j;
-        this.squares[index].x = j * this.squareSize();
-        this.squares[index].y = i * this.squareSize();
-      }
-    }
-    for (let s of this.squares) {
-      s.size = this.squareSize();
-    }
-
-    this.render();
-  },
-
-  // resets the color or anything else back to it's original.
-
-  reset: function () {
-    mouse.reset(); // reset the mouse position so that the last square does not stay white.
-    user.reset();
-    gameStart = false;
-    for (let s of this.squares) {
-      s.color = 'rgba(0,0,0,1)';
-    }
-    this.render();
-    this.render();
-  },
-
-  clear: function () {
-    for (let s of this.squares) {
-      s.color = 'rgba(0,0,0,1)';
-    }
-    this.render();
-    this.render();
-  },
-};
-
-grid.create();
-// grid.random();
 
 // ELEMENTS IN CANVAS
 
-function Square(x, y, size, color) {
+function Square(x, y, size, color = 'rgba(0,0,0,1)') {
   this.x = x;
   this.y = y;
-  // this.originalX = this.x;
-  // this.originalY = this.y;
   this.size = size;
   this.color = color;
-  // this.dx = 1;
-  // this.dy = 1;
-  // this.ds = 1;
+
   this.clicked = false;
   this.draw = () => {
     c.fillStyle = this.color;
@@ -150,25 +49,7 @@ function Square(x, y, size, color) {
     c.fill();
   };
 
-  this.highlight = function () {
-    // Set a new highlight function, that only highlights on square at a time, and does not need to clear the canvas each time.
-    if (
-      mouse.x > this.x &&
-      mouse.x < this.x + this.size &&
-      mouse.y > this.y &&
-      mouse.y < this.y + this.size
-    ) {
-      if (this.color === 'rgba(0,0,0,1)' || this.color === 'black') {
-        this.color = 'rgba(255,255,255,0.1)'; // light white
-      }
-    } else if (this.color === 'rgba(255,255,255,0.1)') {
-      this.color = 'rgba(0,0,0,1)';
-    }
-    this.draw();
-  };
   this.update = () => {
-    // split the update function to multiple functions, each having a different purpose. Having everything in one function makes the logic hard to track the problem with this function is that you can't modify the colors of the squares at all when you are drawing out the squares later.
-
     if (
       mouse.clickedX > this.x &&
       mouse.clickedX < this.x + this.size &&
@@ -189,18 +70,6 @@ function Square(x, y, size, color) {
 
 function clearCanvas() {
   c.clearRect(0, 0, setUpCanvas(), setUpCanvas());
-}
-
-function hide(element) {
-  let htmlElement = document.getElementById(`${element}`);
-
-  if (htmlElement) {
-    if (htmlElement.hasAttribute('hidden')) {
-      htmlElement.removeAttribute('hidden');
-    } else {
-      htmlElement.setAttribute('hidden', true);
-    }
-  }
 }
 
 // hello, this is from the laptop
